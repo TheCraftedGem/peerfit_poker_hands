1cf7e2d34ec2adc8bfa95f9586f80c6d8b6f0560
Emilio <thecraftedgem@tuta.io>
2019-10-05 17:21:07 -0600

    creates first test for evaluateing highest value hand

---
+++ .elixir_ls/build/test/lib/peerfit_poker_hands/ebin/peerfit_poker_hands.app
@@ -0,0 +1,6 @@
+{application,peerfit_poker_hands,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"peerfit_poker_hands"},
+              {modules,['Elixir.PeerfitPokerHands']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- test/peerfit_poker_hands_test.exs
+++ test/peerfit_poker_hands_test.exs
@@ -2,7 +2,16 @@ defmodule PeerfitPokerHandsTest do
   use ExUnit.Case
   doctest PeerfitPokerHands
 
-  test "greets the world" do
-    assert PeerfitPokerHands.hello() == :world
+  test "evaluate hands for highest value hand" do
+
+    player_1 = ["5H", "5C", "6S", "7S", "KD"]
+    player_2 = ["2C", "3S", "8S", "8D", "TD"]
+
+    player_1_second_hand = ["5D", "8C", "9S", "JS", "AC"]
+    player_2_second_hand = ["2C", "5C", "7D", "8S", "QH"]
+
+    assert PeerfitPokerHands.evaluate(player_1, player_2) == "Player 2 Wins!"
+    assert PeerfitPokerHands.evaluate(player_1_second_hand, player_2_second_hand) == "Player 1 Wins"
+    # Possible a function to call after a couple hands and check the score count
   end
 end
