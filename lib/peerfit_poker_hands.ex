41f87296492c0d5e20e9f06b5e72f48a662e33c6
Emilio <thecraftedgem@tuta.io>
2019-10-05 17:30:09 -0600

    Created TODO list for possible strategies

--- lib/peerfit_poker_hands.ex
+++ lib/peerfit_poker_hands.ex
@@ -1,18 +1,12 @@
 defmodule PeerfitPokerHands do
-  @moduledoc """
-  Documentation for PeerfitPokerHands.
-  """
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> PeerfitPokerHands.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def evaluate(player_1_hand, player_2_hand) do
+    # TODOS
+    #   Load Poker.txt File And Parse Into 2 Hands
+    #     Create Parser Module To Handle File
+    #   Use Genserver To Keep Track Of Score And Return Score For Individual Player
+    #     Otherwise use a reduce to keep track instead
+    #   Pattern Match Individual winning hands
   end
 end
